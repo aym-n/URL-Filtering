e2654aa95cb80a2c7a21f654342c9b033c2f9805
Rrrinav <rinavhansa4@gmail.com>
2024-06-07 23:04:39 +0530

    Made a simple interface

--- main.cpp
+++ main.cpp
@@ -2,14 +2,20 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <limits>
 
 using namespace std;
 
 /*
 Polynomial Rolling Hashing Function 
-is a hash function that uses only multiplications and additions
+is a hash function that uses only multiplications and additions.
 
 Hash(s) = ( s[0] + s[1]*p + s[2]*p^2 + ... ) % m
+
+Where:
+- s is the input string
+- p is a prime number, usually a small value like 31
+- m is a large prime number used to take the modulus
 */
 unsigned long long polynomial_hash(const string &s, long long p, long long m)
 {
@@ -30,9 +36,11 @@ Another version of this algorithm (now favored by Bernstein) uses XOR:
 
 hash(i) = hash(i - 1) * 33 ^ str[i]; 
 
-The magic of number 33 (why it works better than many other constants, prime or not) has never been adequately explained. 
-*/
+The magic of number 33 (why it works better than many other constants, prime or not) has never been adequately explained.
+This function uses bitwise operations and arithmetic to compute a hash value efficiently.
 
+Hash(s) = 5381 * 33 + s[i]
+*/
 unsigned long long djb2(const string &s)
 {
     unsigned long long hash = 5381;
@@ -50,11 +58,9 @@ database library.
 It was found to do well in scrambling bits, causing better distribution of the keys
 and fewer splits.
 It also happens to be a good general hashing function with good distribution.
-The actual function (pseudo code) is:
-    for i in i..len(str):
-        hash(i) = hash(i - 1) * 65599 + str[i];
-*/
 
+Hash(s) = hash(i - 1) * 65599 + str[i]
+*/
 unsigned long long sdbm(const string &s)
 {
     unsigned long long hash = 0;
@@ -77,14 +83,12 @@ private:
 public:
     BloomFilter(string filename) : size(0), filename(filename), bits(0)
     {
-        cout << "--- Initializing BloomFilter ---" << endl;
         ifstream file(filename);
         if (file.is_open())
         {
             string line;
             while (getline(file, line))
             {
-                cout << "Added " << line << endl;
                 add(line);
                 size++;
             }
@@ -94,10 +98,6 @@ public:
         {
             cerr << "Unable to open file: " << filename << endl;
         }
-
-        cout << "--- BloomFilter Initialized ---" << endl;
-        cout << "Total Rows Added: " << size << endl;
-        cout << "Total Size: " << bits.size() << " bits" << endl;
     }
 
     void add(const string &s)
@@ -144,19 +144,57 @@ public:
         cout << "Total Negatives: " << negatives << endl;
     }
 
-    bitset<1000001> get_bits()
-    {
-      return bits;
-    }
+    bitset<1000001> get_bits() { return bits; }
 };
 
 int main()
 {
     BloomFilter bf("malicious.csv");
-    bf.test("malicious.csv");
-    getchar();
-    bf.test("benign.csv");
 
-    cout << "All bits set: " << bf.get_bits().all() << endl;
+    // Minimal interactive TUI
+    while (true)
+    {
+        cout << "\n--- Bloom Filter Menu ---\n";
+        cout << "Bitset size: " << bf.get_bits().size() << "\n";
+        cout << "Hash functions used: Polynomial Rolling, DJB2, SDBM\n";
+        cout << "1. Test a file\n";
+        cout << "2. Test a website string\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+
+        int choice;
+        if (!(cin >> choice))
+        {
+            cin.clear();                                          // clear the error flag
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');  // discard invalid input
+            cout << "Invalid input. Please enter a number between 1 and 3.\n";
+            continue;
+        }
+
+        if (choice == 1)
+        {
+            string test_filename;
+            cout << "Enter the file name to test: ";
+            cin >> test_filename;
+            bf.test(test_filename);
+        }
+        else if (choice == 2)
+        {
+            string website;
+            cout << "Enter the website URL to test: ";
+            cin >> website;
+            bool result = bf.contains(website);
+            cout << "The website " << website << " is " << (result ? "possibly malicious." : "not malicious.");
+        }
+        else if (choice == 3)
+        {
+            break;
+        }
+        else
+        {
+            cout << "Invalid choice. Please enter a number between 1 and 3.\n";
+        }
+    }
+
     return 0;
 }
